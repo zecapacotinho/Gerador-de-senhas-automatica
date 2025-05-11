2e42d57984c4af1ed4da280e5664774510dbd5c6
João Carlos Apolinario da Silva <jcasilva1349@gmail.com>
2025-05-11 17:18:23 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -29,11 +29,15 @@ function atualizarValorDoRange(){
 range.addEventListener('input', atualizarValorDoRange)
 
 //Função para o gerador de senha
-function Gerador(){
+function checkBoxSelect(){
+    let pool = []
     const largeLetters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','T','U','V','W','X','Y','Z']
     const smallLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','t','u','v','w','x','y','z']
     const selectValue = [0,1,2,3,4,5,6,7,8,9]
     const addSymbles = ['!','@','$','#','%','&','*','(',')','|','?','/','°']
-    atualizarValorDoRange()
-    
+    if(minValue.checked){pool = pool.concat(smallLetters)}
+    if(maxValue.checked){pool = pool.concat(largeLetters)}
+    if(selectNumber.checked){pool = pool.concat(selectValue)}
+    if(selectSymbles.checked){pool = pool.concat(addSymbles)}
 }
+
